66ecd632fd849cf45604723e8d5076eabd5e6d91
Proking73 <MY_NAME@example.com>
2021-09-25 11:30:03 -0400

    save

--- learning.js
+++ learning.js
@@ -1,21 +1,22 @@
 function nameRun() {
     var You = {
-        Fname:document.getElementById("add_to").value
+        Fname:document.querySelector("#add_to").value
         ,
-        Lname:document.getElementById("add_to").value
+        Lname:document.querySelector("#add_to").value
         , 
         FlName:function fullName(){
             return this.Fname + " " + this.Lname;
             //full name this is owner (user)
         }
+    const borderVhn = document.querySelector("#border")
     };
     function bgColor() {
         //document.querySelector()
-        document.getElementById("mouseover").style.backgroundColor = 'green'
+        document.querySelector("#mouseover").style.backgroundColor = 'green'
     }
 
 
-    document.getElementById("FirstName").innerHTML = "Your name: " + You.Fname;
+    document.querySelector("#FirstName").innerHTML = "Your name: " + You.Fname;
 //document.write("sda" + run);  
 
   }
