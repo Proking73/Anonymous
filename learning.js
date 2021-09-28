7c8b5d8b45af0850ae1d83e92df08d9605665964
Proking73 <77035338+Proking73@users.noreply.github.com>
2021-09-28 12:04:53 -0400

    Update learning.js

--- learning.js
+++ learning.js
@@ -1,13 +1,14 @@
 function nameRun() {
     var You = {
         Fname:document.querySelector("#add_to").value
-        ,
-        Lname:document.querySelector("#add_to").value
-        , 
-        FlName:function fullName(){
-            return this.Fname + " " + this.Lname;
-            //full name this is owner (user)
-        }
+//         ,
+//         Lname:document.querySelector("#add_to").value
+//         , 
+//         FlName:function fullName(){
+//             return this.Fname + " " + this.Lname;
+//             //full name this is owner (user)
+    document.getElementById("FirstName").innerHTML = You.Fname;    
+    }
     const borderVhn = document.querySelector("#border")
     };
     function bgColor() {
