f1124729076c93758457155d5a86c5fb1b630072
Proking73 <MY_NAME@example.com>
2021-10-02 17:17:11 -0400

    whoo

--- learning.js
+++ learning.js
@@ -1,31 +1,44 @@
-function nameRun() {
-    var You = {
-        Fname:document.querySelector("#add_to").value
-        ,
-        Lname:document.querySelector("#add_to").value
-        , 
-    }
-    document.querySelector("#FirstName").innerHTML = "Hi! " + You.Fname;
-        //FlName:function fullName(){
-        //    return this.Fname + " " + this.Lname;
-        //    //full name this is owner (user)
-        //}
-    //const borderVhn = document.querySelector("#border")
-    //};
-    //function bgColor() {
-    //    //document.querySelector()
-    //    document.querySelector("#mouseover").style.backgroundColor = 'green'
-    //}
-function GuessNumbN() {
-var randoNum = 1;
-if (document.querySelector("#rando").value = randoNum){
-    document.querySelector("#Guess").innerHTML = "test";
-} else {
-    document.querySelector("#Guess").innerHTML = "test";
-}
-}
+//function nameRun() {
+//    var You = {
+//        Fname:document.querySelector("#add_to").value
+//        ,
+//        Lname:document.querySelector("#add_to").value
+//        , 
+//    }
+//    document.querySelector("#FirstName").innerHTML = "Hi! " + You.Fname;
+//        FlName:function fullName(){
+//            return this.Fname + " " + this.Lname;
+//            //full name this is owner (user)
+//        }
+//    const borderVhn = document.querySelector("#border");
+//    function bgColor() {
+//        //document.querySelector()
+//        document.querySelector("#mouseover").style.backgroundColor = 'green'
+//    }
+//function GuessNumbN() {
+//var randoNum = 1;
+//if (document.querySelector("#rando").value = randoNum){
+//    document.querySelector("#Guess").innerHTML = "test";
+//} else {
+//    document.querySelector("#Guess").innerHTML = "test";
+//}
+//}
 
-//    document.querySelector("#FirstName").innerHTML = "Your name: " + You.Fname;
-//document.write("sda" + run);  
+////    document.querySelector("#FirstName").innerHTML = "Your name: " + You.Fname;
+////document.write("sda" + run);  
+function windoweeOpen() {
+    window.open('https://proking73.github.io/Anonymous/');
+  }
 
+
+  function windoweeClose() {
+    window.close();
+  }
+
+  function windoweeResize() {
+    window.resizeTo(
+        window.screen.availWidth / 2,
+        window.screen.availHeight / 2
+      );
+      //can only resize the window open tabs, cant resize if have multiple tabs.
   }
